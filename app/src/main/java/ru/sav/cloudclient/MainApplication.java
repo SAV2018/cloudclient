dec99b9dfe1358f2f7f403ecd7c28b720baed7ee
SAV <arconar@mail.ru>
2018-09-18 00:36:12 +0300

    Initial commit: - apply realm

--- app/src/main/java/ru/sav/cloudclient/MainApplication.java
+++ app/src/main/java/ru/sav/cloudclient/MainApplication.java
@@ -2,10 +2,17 @@ package ru.sav.cloudclient;
 
 import android.app.Application;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class MainApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Realm.init(this);
+        RealmConfiguration config = new RealmConfiguration.Builder().build();
+        Realm.setDefaultConfiguration(config);
     }
 }
